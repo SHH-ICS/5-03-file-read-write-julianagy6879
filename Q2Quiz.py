3396f51d0af7aee99a675abad03c9192d6e8a126
Julia Nagy <julia.nagy@stu.ocsb.ca>
2024-12-13 10:22:14 -0500

    Update Q2Quiz.py

--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,20 +1,20 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
 score = 0
-
 questions = [
-    ["Where were french fries originally made?", "a", ["a) Belgium", "b) France", "c) America", "d) Italy"]],
+    {"q": "Where were french fries originally made?", "c": {"a": "Belgium", "b": "France", "c": "America", "d": "Italy"}, "a": "a"}
 ]
-
 for question in questions:
-    print(question[0])
-    for option in question[2]:
-        print(option)
-    answer = input("Answer (a/b/c/d): ").lower()
-    if answer == question[1]:
+    print(question["q"])
+   
+    print("a) " + question["c"]["a"])
+    print("b) " + question["c"]["b"])
+    print("c) " + question["c"]["c"])
+    print("d) " + question["c"]["d"])
+    answer = input("Answer (a/b/c/d): ")
+    if answer == question["a"]:
         print("Correct!\n")
         score += 1
     else:
         print("Wrong!\n")
-
-print(f"Score: {score}/{len(questions)}")
+print("Your final score is: " + str(score))
