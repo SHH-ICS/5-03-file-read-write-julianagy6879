427b3f26a49badf3756856e7ca7e0772ad742d7f
Julia Nagy <julia.nagy@stu.ocsb.ca>
2024-12-13 09:59:58 -0500

    Update Q1Questions.py

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,7 +1,11 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
-q = "Where were french fries originally made?"
-options = "A) Belgium B) France C) America D) Italy"
-print(q, "\n" + options)
-answer = input("Enter your answer (A/B/C/D): ").strip().upper()
-print("Correct!" if answer == "A" else "Incorrect. The correct answer is A) Belgium.")
+filehandle = open("testFile.txt",'w')
+Question = input("What is your question?" )
+filehandle.write(Question +'\n')
+for i in range (4):
+answer = input ("Options:")
+filehandle.write(answer +'\n')
+answer = input("Correct answer?")
+filehandle.write(answer +'\n')
+filehandle.close()
